969afc3d3281fc0a4cd420ba966c225725d8d38e
MrRincon <ar1564@live.mdx.ac.uk>
2023-12-26 22:40:25 +0000

    Procesors, resources and availables added

--- bankersAlgorithm.bash
+++ bankersAlgorithm.bash
@@ -1,41 +1,73 @@
 #Coursework 1 - M00774667 - Alam Rincon Rodrigues
 #Bankers Algorithm for Deadlock Avoidance using Bash
-#!/bin/bash
+#!/bin/sh
 start(){
-    #Check that inputs are correct
+    #Get processors
     getProcessors
-    echo "There is $numProcessors processor(s)"
+    num_processors=$numProcessors
+    echo "There is $num_processors processor(s)"
+    #Get resources
     getResources
-    echo "There is $numResources resource(s)"
+    num_resources=$numResources
+    echo "There is $num_resources resource(s)"
+    #Initialise arrays
+    available=();
+    allocated=()
+    maxDemand=()
+    need=()
+    #Get Available resources
+    getAvailable
+    for (( i=0; i<$numResources; i++ ))
+    do
+        echo ${available[$i]}
+        done
 }
 getProcessors(){
-    read -p "Enter the number of processors(min 1 and max 5): " numProcessors
+    read -p "Enter the number of processors(min 1 and max 10): " numProcessors
+    #Processors input check
     if [ -z $numProcessors ] 
     then
-        # echo "Please enter some processors"
         getProcessors
-    elif [ $numProcessors -lt 1 ] || [ $numProcessors -gt 5 ] 
+    elif [ $numProcessors -ge 1 ] && [ $numProcessors -le 10 ] 
     then
-        # echo "Minimum 1 and maximum 10 processors. try again"
-        getProcessors
-    else 
-        # echo "There is $numProcessors processor(s)"
         return $numProcessors
+    else 
+        getProcessors
     fi
 }
 getResources(){
-    read -p "Enter the number of resources: " numResources
+    read -p "Enter the number of resources(min 3 and max 5): " numResources
+    #Resources input check
     if [ -z $numResources ] 
     then
-        # echo "Please enter some resources"
         getResources 
-    elif [ $numResources -lt 1 ] || [ $numResources -gt 3 ] 
+    elif [ $numResources -ge 3 ] && [ $numResources -le 5 ] 
     then
-        # echo "Minimum 1 and maximum 3 resources. try again"
-        getResources
-    else 
-        # echo "There is $numResources resources(s)"
         return $numResources
+    else 
+        getResources
+    fi
+}
+getAvailable(){
+    echo "Enter the available resources: "
+    for (( i=0; i<$num_resources; i++ ))
+    do
+        echo "Resource $((i+1))"
+        fillResource
+        available[$i]=$avResValue
+        done
+    return $available
+}
+fillResource(){
+    read -p "Enter value: " avResValue 
+    if [ -z $avResValue ]
+    then 
+        fillResource
+    elif [ $avResValue -ge 0 ]
+    then 
+        return $avResValue
+    else
+        fillResource
     fi
 }
 start
