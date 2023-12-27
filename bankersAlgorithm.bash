cb886046d1bc5407eaa44f28b7e6c1444148080e
MrRincon <ar1564@live.mdx.ac.uk>
2023-12-27 13:58:41 +0000

    allocated resources added but still working on printing them in a linear way

--- bankersAlgorithm.bash
+++ bankersAlgorithm.bash
@@ -1,35 +1,34 @@
+#!/bin/bash
 #Coursework 1 - M00774667 - Alam Rincon Rodrigues
 #Bankers Algorithm for Deadlock Avoidance using Bash
-#!/bin/sh
 start(){
     #Get processors
     getProcessors
-    num_processors=$numProcessors
-    echo "There is $num_processors processor(s)"
+    echo "There is $numProcessors processor(s)"
     #Get resources
     getResources
-    num_resources=$numResources
-    echo "There is $num_resources resource(s)"
+    echo "There is $numResources resource(s)"
     #Initialise arrays
-    available=();
-    allocated=()
+    declare -a available
+    declare -A allocated
     maxDemand=()
     need=()
     #Get Available resources
     getAvailable
-    for (( i=0; i<$numResources; i++ ))
-    do
-        echo ${available[$i]}
-        done
+    #Get allocated resources for each processor
+    getAllocated
+    #Print Processors, available resources and allocated resources
+    echo "AVAILABLE RESOURCES: ${available[*]}"
+    # for (( i=0; i<$numProcessors; i++ )); do 
+    #     echo "P$((i+1)) "  
+    # done
 }
 getProcessors(){
     read -p "Enter the number of processors(min 1 and max 10): " numProcessors
     #Processors input check
-    if [ -z $numProcessors ] 
-    then
+    if [ -z $numProcessors ]; then
         getProcessors
-    elif [ $numProcessors -ge 1 ] && [ $numProcessors -le 10 ] 
-    then
+    elif [ $numProcessors -ge 1 ] && [ $numProcessors -le 10 ]; then
         return $numProcessors
     else 
         getProcessors
@@ -38,11 +37,9 @@ getProcessors(){
 getResources(){
     read -p "Enter the number of resources(min 3 and max 5): " numResources
     #Resources input check
-    if [ -z $numResources ] 
-    then
+    if [ -z $numResources ]; then
         getResources 
-    elif [ $numResources -ge 3 ] && [ $numResources -le 5 ] 
-    then
+    elif [ $numResources -ge 3 ] && [ $numResources -le 5 ]; then
         return $numResources
     else 
         getResources
@@ -50,24 +47,43 @@ getResources(){
 }
 getAvailable(){
     echo "Enter the available resources: "
-    for (( i=0; i<$num_resources; i++ ))
-    do
-        echo "Resource $((i+1))"
-        fillResource
-        available[$i]=$avResValue
-        done
+    for (( i=0; i<$numResources; i++ )); do
+        echo "Resource $((i+1))" 
+        fillAvaiResource
+        available[$i]=$avaiResValue
+    done
     return $available
 }
-fillResource(){
-    read -p "Enter value: " avResValue 
-    if [ -z $avResValue ]
-    then 
-        fillResource
-    elif [ $avResValue -ge 0 ]
-    then 
-        return $avResValue
+fillAvaiResource(){
+    read -p "Enter value: " avaiResValue 
+    if [ -z $avaiResValue ]; then 
+        fillAvaiResource
+    elif [ $avaiResValue -ge 0 ]; then 
+        return $avaiResValue
     else
-        fillResource
+        fillAvaiResource
     fi
 }
-start
+getAllocated(){
+    echo "Enter the allocated resources of each processor:"
+    for (( i=0; i<$numProcessors; i++ )); do 
+        echo "PROCESSOR $((i+1))"
+        for (( j=0; j<$numProcessors; j++ )); do 
+            echo "Resource $((j+1))" 
+            fillAllocResource
+            allocated[$i,$j]=$allocResValue
+        done
+    done
+    return $allocated
+}
+fillAllocResource(){
+    read -p "Enter value: " allocResValue
+    if [ -z $allocResValue ]; then 
+        fillAllocResource
+    elif [ $allocResValue -ge 0 ]; then
+        return $allocResValue
+    else
+        fillAllocResource
+    fi
+}
+start 
